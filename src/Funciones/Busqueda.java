1eb358de1813b8e8ea9c6a867fc80b65d38e6886
MigueMonti <mmontilla@correo.unimet.edu.ve>
2024-03-10 23:17:53 -0400

    Funcion registro

--- src/Funciones/Busqueda.java
+++ src/Funciones/Busqueda.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Funciones;
 
-/**
- *
- * @author nicolepinto
- */
 public class Busqueda {
+    private String name;
+    private String lastname;
+    private String list_name;
+    private String list_lastname;
+    private int num_hab;
+
+    public Busqueda(String name, String lastname, String list_name, String list_lastname, int num_hab) {
+        this.name = name;
+        this.lastname = lastname;
+        this.list_name = list_name;
+        this.list_lastname = list_lastname;
+        this.num_hab = num_hab;
+    }
+     
+    public void registro(){
+        if (lastname == list_lastname && name == list_name)  {
+            JOptionPane.showMessageDialog(null, num_hab);
+        } else {
+            JOptionPane.showMessageDialog(null, "Nuevo cliente.");
+        }
+    }
     
 }
