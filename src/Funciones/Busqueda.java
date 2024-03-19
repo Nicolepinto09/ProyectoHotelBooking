9e414945d82882bc2f11915cce93271f8cf90248
MigueMonti <mmontilla@correo.unimet.edu.ve>
2024-03-19 14:13:01 -0400

    Clase Busqueda Variables a Espanol

--- src/Funciones/Busqueda.java
+++ src/Funciones/Busqueda.java
@@ -3,13 +3,13 @@ package Funciones;
 import javax.swing.JOptionPane;
 
 public class Busqueda {
-    private String name;
-    private String lastname;
-    private String list_name;
-    private String list_lastname;
+    private String nombre;
+    private String apellido;
+    private String lista_nombre;
+    private String lista_apellido;
     private int num_hab;
     private String ci;
-    private String list_ci;
+    private String lista_ci;
     private String email;
     private String genero;
     private String tipo_hab;
@@ -17,19 +17,19 @@ public class Busqueda {
     private String llegada;
     private String salida;
 
-    public Busqueda(String name, String lastname, String list_name, String list_lastname, int num_hab) {
-        this.name = name;
-        this.lastname = lastname;
-        this.list_name = list_name;
-        this.list_lastname = list_lastname;
+    public Busqueda(String nombre, String apellido, String lista_nombre, String lista_apellido, int num_hab) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.lista_nombre = lista_nombre;
+        this.lista_apellido = lista_apellido;
         this.num_hab = num_hab;
     }
 
-    public Busqueda(String list_name, String list_lastname, String ci, String list_ci, String email, String genero, String tipo_hab, String celular, String llegada, String salida) {
-        this.list_name = list_name;
-        this.list_lastname = list_lastname;
+    public Busqueda(String lista_nombre, String lista_apellido, String ci, String lista_ci, String email, String genero, String tipo_hab, String celular, String llegada, String salida) {
+        this.lista_nombre = lista_nombre;
+        this.lista_apellido = lista_apellido;
         this.ci = ci;
-        this.list_ci = list_ci;
+        this.lista_ci = lista_ci;
         this.email = email;
         this.genero = genero;
         this.tipo_hab = tipo_hab;
@@ -39,21 +39,21 @@ public class Busqueda {
     }
      
     public void clientes(){
-        if (lastname == list_lastname && name == list_name)  {
-            JOptionPane.showMessageDialog(null, "El cliente " + name + " " + lastname + " se encuentra alojado en la habitación " + num_hab);
+        if (apellido == lista_apellido && nombre == lista_nombre)  {
+            JOptionPane.showMessageDialog(null, "El cliente " + nombre + " " + apellido + " se encuentra alojado en la habitación " + num_hab);
         }
     }
     
     public void reservacion() {
         boolean r = true;
         for (int i = 1; i < ci.length()+1; i++) {
-            if (ci.charAt(ci.length()-i) != list_ci.charAt(list_ci.length()-i)) {
+            if (ci.charAt(ci.length()-i) != lista_ci.charAt(lista_ci.length()-i)) {
                 r = false;
                 break;
             }
         }
         if (r) {
-            JOptionPane.showMessageDialog(null, "- Cliente: " + list_name + " " + list_lastname + "\n- C.I. " + list_ci +
+            JOptionPane.showMessageDialog(null, "- Cliente: " + lista_nombre + " " + lista_apellido + "\n- C.I. " + lista_ci +
                     "\n- Correo: " + email + "\n- Género: " + genero + "\n- Tipo de habitación: " + tipo_hab + 
                     "\n- Teléfono: " + celular + "\n- Fecha de entrada: " + llegada + "\n- Fecha de salida: " + salida);
         }
